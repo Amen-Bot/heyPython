b99c24f380d23a464d63a9ee0050f00a6e044719
Amen Aghayedo <amenaghayedo@gmail.com>
2023-01-09 10:57:38 +0100

    Recently Updated

---
+++ New/Use.py
@@ -0,0 +1,29 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+import time
+import selenium
+from selenium.common.exceptions import InvalidArgumentException
+from selenium.common.exceptions import SessionNotCreatedException
+from selenium.common.exceptions import WebDriverException
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['automationName'] = 'UiAutomator2Options()'
+desired_caps['platformVersion'] = '10'
+desired_caps['deviceName'] = 'Galaxy A20DD8D'
+desired_caps['app'] = ('C:/Users/user/Downloads/mobileApp.apk')
+desired_caps['appPackage'] = 'co.threewin.wowzi.app'
+desired_caps['appActivity'] = 'com.wazi.MainActivity'
+
+driver =webDriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+
+ele_id = driver.find_element(AppiumBy.xpath, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                             "/android.widget.FrameLayout/android.widget.LinearLayout/android.widget"
+                                             ".FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android"
+                                             ".view.ViewGroup/android.view.ViewGroup["
+                                             "3]/android.view.ViewGroup/android.widget.HorizontalScrollView/android"
+                                             ".view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android"
+                                             ".view.ViewGroup[1]/android.widget.TextView")
+ele_id.click()
+
+
